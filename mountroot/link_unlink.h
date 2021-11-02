2f9d5f7328da2b042d6db90b75af52bf53e610df
JoeSteeb <josteeb@gmail.com>
2021-11-02 15:19:12 -0700

    added headers for level1

---
+++ mountroot/link_unlink.h
@@ -0,0 +1,7 @@
+#include "type.h"
+#include "util.h"
+
+#ifndef LINK_UNLINK_H
+#define LINK_UNLINK_H
+
+#endif
---
+++ mountroot/rmdir.h
@@ -0,0 +1,7 @@
+#include "type.h"
+#include "util.h"
+
+#ifndef RMDIR_H
+#define RMDIR_H
+
+#endif
