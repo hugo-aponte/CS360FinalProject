503e17b1d86024b7db61d05945f34bb2a4ffacb0
hugo-aponte <hjraponte@live.com>
2021-11-02 20:05:15 -0700

    Started implementing the misc.c file. Not everything has been added yet

---
+++ mountroot/misc.c
@@ -0,0 +1,42 @@
+#include "type.h"
+
+int stat(int dev, char *pathname)
+{
+    struct stat myst;
+    int ino;
+    MINODE *mip;
+
+    ino = getino(pathname);
+    mip = iget(dev, ino);
+
+    myst.st_dev = dev;
+    myst.st_ino = ino;
+
+    myst.st_blocks = mip->INODE.i_blocks;
+    myst.st_gid = mip->INODE.i_gid;
+    myst.st_uid = mip->INODE.i_uid;
+    myst.st_size = mip->INODE.i_size;
+    myst.st_mode = mip->INODE.i_mode;
+    myst.st_nlink = mip->INODE.i_links_count;
+    
+    iput(mip);
+}
+
+int chmod(int dev, char *pathname, u16 mode)
+{
+    int ino;
+    MINODE *mip;
+
+    ino = getino(pathname);
+    mip = iget(dev, ino);
+
+    mip->INODE.i_mode |= mode;
+    mip->dirty = 1;
+
+    iput(mip);
+}
+
+// int utime(char *pathname)
+// {
+
+// }
