8ded2e7f11bc010f743498641194895787f8eb01
JoeSteeb <josteeb@gmail.com>
2021-11-12 03:43:41 -0800

    added checking for empty file

--- mountroot/rmdir.c
+++ mountroot/rmdir.c
@@ -84,7 +84,6 @@ int rm_child(MINODE *pip, char *fName, int ino)
 
         if (!strcmp(name, fName))
         {
-            printf("hello");
             prevDp->rec_len += dp->rec_len;
         }
 
@@ -169,41 +168,38 @@ int myRmdir()
     MINODE *temp;
 
     //check all inode blocks, don't quite understand why, but rmdir example code does it.
-    for (int i = 2; i < 12; i++)
+    for (int i = 0; i < 12; i++)
     {
         // check if direct block where our directories are is valid
-        if (mip->INODE.i_block[i])
-        {
-            printf("rmdir: cannot rmdir, dir not empty\n");
-            return -1;
-        }
+        if (!mip->INODE.i_block[i])
+            break;
 
         // set block content to buf
         get_block(dev, mip->INODE.i_block[i], buf);
         cp = buf;
         dp = (DIR *)buf;
 
-        // traverse directories utilizing dp and cp
-        // while (cp < &buf[BLKSIZE])
-        // {
-        //     // handle directory name properly
-        //     strncpy(name, dp->name, dp->name_len);
-        //     name[dp->name_len] = 0;
+        // directories utilizing dp and cp
+        while (cp < &buf[BLKSIZE])
+        {
+            // handle directory name properly
+            strncpy(name, dp->name, dp->name_len);
+            name[dp->name_len] = 0;
 
-        //     // set inode number to that of the current directory
-        //     ino = dp->inode;
+            // set inode number to that of the current directory
+            ino = dp->inode;
 
-        //     if (name[0] != 0)
-        //     {
-        //         printf("cannot rmdir, dir not empty\n");
-        //         return 1;
-        //     }
+            if (name[0] != 0 && strcmp(name, ".") && strcmp(name, ".."))
+            {
+                printf("cannot rmdir, %s not empty\n", fileName);
+                printf("%s contains %s and posibly more\n", fileName, name);
+                return 1;
+            }
 
-        //     // advance to next record and set directory pointer to next directory
-        //     cp += dp->rec_len;
-        //     dp = (DIR *)cp;
-        //     iput(temp);
-        // }
+            // advance to next record and set directory pointer to next directory
+            cp += dp->rec_len;
+            dp = (DIR *)cp;
+        }
     }
 
     //dir is now confirmed not to be empty, and rmdir can begin
